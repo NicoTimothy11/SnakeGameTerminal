d23f77ae5a24735cf0f3fe5f423cdde37f47e35e
Nico Timothy <142752571+NicoTimothy11@users.noreply.github.com>
2025-01-09 19:29:00 +0800

    Created a grid

--- main.cpp
+++ main.cpp
@@ -11,8 +11,13 @@ int main() {
     vector<int> snakeBody = {0, 0, 0};
 
     vector<vector<char>> Grid(rows, vector<char>(columns, '*'));
+
     for(int i = 0; i < rows; i++) {
         for(int j = 0; j < columns; j++) {
+            Grid[i][9] = '#';
+            Grid[9][i] = '#';
+            Grid[j][0] = '#';
+            Grid[0][j] = '#';
             cout << Grid[i][j] << " ";
         }
         cout << endl;
