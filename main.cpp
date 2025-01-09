e447c7d52f5bd69f5c7cdc36cc6d88799054b1ce
Nico Timothy <142752571+NicoTimothy11@users.noreply.github.com>
2025-01-09 19:15:01 +0800

    Created a grid

--- main.cpp
+++ main.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
 #include <vector>
-#include <conio.h>
 #include <string>
 
 using namespace std;
 
 int main() {
 
-    const int rows = 10;
-    const int columns = 10;
+    int rows = 10, columns = 10;
 
-        char* array[rows][columns] = {'*'};
+    vector<int> snakeBody = {0, 0, 0};
 
-        for (int i = 0; i < rows; i++) {
-            for(int j = 0; j < columns; j++)
-            {
-                cout << array[i][j] << " ";
-            }
-            cout << endl;
+    vector<vector<char>> Grid(rows, vector<char>(columns, '*'));
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < columns; j++) {
+            cout << Grid[i][j] << " ";
         }
+        cout << endl;
+    }
+
     return 0; 
 }
