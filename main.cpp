6506ee9f6b6d8a7fa0d5067fb27fb52d52738c0b
Nico Timothy <142752571+NicoTimothy11@users.noreply.github.com>
2025-01-09 18:51:00 +0800

    Reset

--- main.cpp
+++ main.cpp
@@ -1,20 +1,16 @@
 #include <iostream>
 #include <vector>
 #include <conio.h>
+#include <string>
 
 using namespace std;
 
 int main() {
-    bool gameOver = false;
-    char ch;
 
     const int rows = 10;
     const int columns = 10;
 
-        int array[rows][columns] = { 0 };
-        int x = 5;
-        int y = 3;
-        array[x][y] = 3;
+        char* array[rows][columns] = {'*'};
 
         for (int i = 0; i < rows; i++) {
             for(int j = 0; j < columns; j++)
@@ -23,42 +19,5 @@ int main() {
             }
             cout << endl;
         }
-
-    while(gameOver == false) {
-
-        cout << "Enter a character to move: ";
-        ch = getch();
-        
-        cout << endl;
-        while(ch = 'd') {
-            ch = getch();
-
-            for (int i = 0; i < rows; i++) {
-                for(int j = 0; j < columns; j++)
-                {
-                    array[i][j] = array[i][j + 1];
-                    cout << array[i][j] << " ";
-                }
-                cout << endl;
-            }
-
-            if(ch == 'a') {
-            break;
-            }  
-        }
-              
-
-
-        /*for (int i = 0; i < rows; i++) {
-            for(int j = 0; j < columns; j++) {
-                cout << array[i][j] << " ";
-            }
-            cout << endl;
-        } */
-        
-        if(gameOver = true) {
-            break;
-        }
-    }
     return 0; 
 }
