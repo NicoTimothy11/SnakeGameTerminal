ed97e92cfa5abcf542674a6045d2549d90e8e003
Nico Timothy <142752571+NicoTimothy11@users.noreply.github.com>
2025-01-10 20:21:45 +0800

    Recent Changes

--- main.cpp
+++ main.cpp
@@ -1,31 +1,52 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <conio.h>
 
 using namespace std;
 
 int main() {
 
     int rows = 10, columns = 10;
+    bool growing, gameOver;
+    char ch;
 
-    vector<char> snakeBody = {'0', '0', '0'};
+    gameOver = false;
 
+    vector<char> snakeBody = {'H', '0', '0'};
     vector<vector<char>> Grid(rows, vector<char>(columns, '*'));
 
+    while(gameOver == false) {
+    
     for(int i = 0; i < rows; i++) {
         for(int j = 0; j < columns; j++) {
-            Grid[i][9] = '#';
-            Grid[9][i] = '#';
-            Grid[j][0] = '#';
-            Grid[0][j] = '#';
+                Grid[i][9] = '#';
+                Grid[9][i] = '#';
+                Grid[j][0] = '#';
+                Grid[0][j] = '#';
 
-            Grid[2][2] = snakeBody[0];
-            Grid[2][3] = snakeBody[1];
-            Grid[2][4] = snakeBody[2];
+                Grid[2][2] = snakeBody[2];
+                Grid[2][3] = snakeBody[1];
+                Grid[2][4] = snakeBody[0];
 
-            cout << Grid[i][j] << " ";
+                cout << Grid[i][j] << " ";
+            }
+            cout << endl;
         }
+
+        cout << "Enter a key: ";
         cout << endl;
+        ch = getch();
+
+        if(ch == 'd') {
+            Grid[2][2] = snakeBody[2];
+            Grid[2][3] = snakeBody[1];
+            Grid[2][4] = snakeBody[0];
+        }
+        if(ch == 'a') {
+            gameOver = true;
+            break;
+        }
     }
 
     return 0; 
