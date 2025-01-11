5a60fb1698219a2c68025c95a471deaee1952239
Nico Timothy <142752571+NicoTimothy11@users.noreply.github.com>
2025-01-11 19:20:01 +0800

    Added a new changes to the code

--- main.cpp
+++ main.cpp
@@ -1,53 +1,29 @@
 #include <iostream>
 #include <vector>
-#include <string>
+#include <cstdlib.h>
 #include <conio.h>
 
 using namespace std;
 
+class Grid {
+    public:
+        int rows = 10;
+        int columns = 10;
+};
+
+class Snake {
+    public:
+        int length;
+};
+
+class Game {
+    public:
+        void HandleUpdate();
+        void DrawBoard();
+        void ClearScreen();
+};
+
 int main() {
-
-    int rows = 10, columns = 10;
-    bool growing, gameOver;
-    char ch;
-
-    gameOver = false;
-
-    vector<char> snakeBody = {'H', '0', '0'};
-    vector<vector<char>> Grid(rows, vector<char>(columns, '*'));
-
-    while(gameOver == false) {
-    
-    for(int i = 0; i < rows; i++) {
-        for(int j = 0; j < columns; j++) {
-                Grid[i][9] = '#';
-                Grid[9][i] = '#';
-                Grid[j][0] = '#';
-                Grid[0][j] = '#';
-
-                Grid[2][2] = snakeBody[2];
-                Grid[2][3] = snakeBody[1];
-                Grid[2][4] = snakeBody[0];
-
-                cout << Grid[i][j] << " ";
-            }
-            cout << endl;
-        }
-
-        cout << "Enter a key: ";
-        cout << endl;
-        ch = getch();
-
-        if(ch == 'd') {
-            Grid[2][2] = snakeBody[2];
-            Grid[2][3] = snakeBody[1];
-            Grid[2][4 + 1] = snakeBody[0];
-        }
-        if(ch == 'a') {
-            gameOver = true;
-            break;
-        }
-    }
 
     return 0; 
 }
